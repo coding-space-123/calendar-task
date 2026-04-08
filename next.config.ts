cc45d7317670241363b09b691153311076d9566e
Vaishnavi Gupta <vaishnavigupta@Vaishnavis-MacBook-Air.local>
2026-04-08 14:41:05 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextconfig = {
+  output: 'export', 
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+export default nextconfig;
